8bb378e8e4661d0abfd6920a616f289b89285420
Paolo Ragudo <ragudopaolo@gmail.com>
2026-08-20 18:15:11 +0800

    program.cs adjustments for deployment, created Dockerfile

--- TicTacToeOnlineProject/TicTacToeOnline/Program.cs
+++ TicTacToeOnlineProject/TicTacToeOnline/Program.cs
@@ -5,6 +5,16 @@ using TicTacToeOnline.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var envOrigins = Environment.GetEnvironmentVariable("ALLOWED_ORIGINS");
+
+var allowedOrigins = !string.IsNullOrWhiteSpace(envOrigins)
+    ? envOrigins.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+    : [
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "http://192.168.18.152:5173"
+      ];
+
 builder.Services.AddSingleton<IGameStore, InMemoryGameStore>();
 builder.Services.AddTransient<BoardService>();
 builder.Services.AddSignalR()
@@ -20,16 +30,15 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(policy =>
     {
         policy
-            .WithOrigins(
-                "http://localhost:3000",
-                "http://localhost:5173",
-                "http://192.168.18.152:5173"
-            )
+            .WithOrigins(allowedOrigins)
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials();
     });
 });
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 var app = builder.Build();
 
