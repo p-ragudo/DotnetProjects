d0cd74980561ce235c04257172a05c127500c4de
Paolo Ragudo <ragudopaolo@gmail.com>
2026-08-20 19:28:18 +0800

    hubUrl frontend added fallback url

--- TicTacToeOnlineProject/Frontend/src/context/SignalRContext.tsx
+++ TicTacToeOnlineProject/Frontend/src/context/SignalRContext.tsx
@@ -41,7 +41,7 @@ export const SignalRProvider: React.FC<{ children: React.ReactNode }> = ({ child
     }
   }, [startConnection]);
 
-  const hubUrl = import.meta.env.VITE_SIGNALR_HUB_URL
+  const hubUrl = import.meta.env.VITE_SIGNALR_HUB_URL || 'http://192.168.18.152:5267/hubs/game';
 
   useEffect(() => {
     const newConnection = new signalR.HubConnectionBuilder()
